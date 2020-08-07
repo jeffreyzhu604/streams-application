9016da7dacede034f32e1c119cdadf5a4b4aa3c3
Jeffrey Zhu <jeffreyzhuu@gmail.com>
2020-08-06 18:14:05 -0700

    refacted server code and code to connect to postgresql database, added docker dev file for server and rtmp server, and added docker compose file

---
+++ server/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    pgUser: process.env.PGUSER,
+    pgHost: process.env.PGHOST,
+    pgDatabase: process.env.POSTGRES_DB,
+    pgPassword: process.env.POSTGRES_PASSWORD,
+    pgPort: process.env.PGPORT
+};
